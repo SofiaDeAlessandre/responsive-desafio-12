6e42fb0feab350a1593b0fe54eb877e4a3040c37
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-01-22 04:16:15 -0300

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,10 @@ export default {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                ejercicioA: resolve(__dirname, 'pages/ejercicio-a/ejercicio-a.html'),
-                ejercicioB: resolve(__dirname, 'pages/ejercicio-b/ejercicio-b.html'),
-                ejercicioC: resolve(__dirname, 'pages/ejercicio-c/ejercicio-c.html'),
-                ejercicioD: resolve(__dirname, 'pages/ejercicio-d/ejercicio-d.html'),
+                ejercicioA: resolve(__dirname, 'pages/ejercicioA/ejercicio-a.html'),
+                ejercicioB: resolve(__dirname, 'pages/ejercicioB/ejercicio-b.html'),
+                ejercicioC: resolve(__dirname, 'pages/ejercicioC/ejercicio-c.html'),
+                ejercicioD: resolve(__dirname, 'pages/ejercicioD/ejercicio-d.html'),
             }
         }
     }
