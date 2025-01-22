a6a8ef700bb693cfa1f087268374471e6aefa89d
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2025-01-22 01:14:27 -0300

    add exercise-a

--- pages/ejercicioA/main-a.js
+++ pages/ejercicioA/main-a.js
@@ -1,1 +1,9 @@
 import './style-a.css'
+
+let links = document.getElementsByClassName('link')
+
+
+    for (let link of links){
+        link.addEventListener("click", () => {
+           link.classList.add('violet') })
+    }
